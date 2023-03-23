0b513a27a9c19fd6041161a2730aa79d7543f006
Elvee Bolt <boltelvee@gmail.com>
2023-03-23 14:11:50 +0200

    Edited queryset in service list view

--- service/views.py
+++ service/views.py
@@ -1,5 +1,7 @@
+import datetime
 from django.views.generic import DetailView, ListView
 from .models import Service
+from master.models import Master, Calendar
 
 
 # Create your views here.
@@ -11,6 +13,25 @@ class ServiceListView(ListView):
         'title': 'Услуги',
         'subtitle': 'Lorem Ipsum'
     }
+
+    # def get_queryset(self):
+    #     master_calendar = super().get_queryset().filter(
+    #         date__gte=datetime.date.today(),
+    #         date__lte=datetime.date.today() + datetime.timedelta(days=7)
+    #     ).values('master')
+    #     print(master_calendar)
+    #
+    #     master = Master.objects.filter(id__in=master_calendar, status=True).values('service').distinct()
+    #     print(master)
+    #
+    #     return Service.objects.filter(id__in=master)
+
+    def get_queryset(self):
+        queryset = super().get_queryset().filter(
+            master__calendar__date__range=(datetime.date.today(), datetime.date.today() + datetime.timedelta(days=7))
+        ).distinct()
+
+        return queryset
 
 
 class ServiceDetailView(DetailView):
