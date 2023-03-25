db1d95d154ad058c95f0991e7dc8b8fb1c9538a7
Elvee Bolt <boltelvee@gmail.com>
2023-03-25 12:28:04 +0200

    Added description field for service model

--- service/models.py
+++ service/models.py
@@ -4,6 +4,7 @@ from django.db import models
 # Create your models here.
 class Service(models.Model):
     title = models.CharField(max_length=255, verbose_name='Название услуги')
+    description = models.TextField(verbose_name='Описание услуги')
     time = models.IntegerField(verbose_name='Продолжительность (в мин.)')
     price = models.IntegerField(verbose_name='Стоимость услуги')
 
