4c9deb5c53bd19eaa22959dedc770195ed67cfd4
Elvee Bolt <boltelvee@gmail.com>
2023-04-08 14:36:23 +0300

    Added TestCase for manager calendar

--- user/tests.py
+++ user/tests.py
@@ -22,7 +22,7 @@ class ManagerTestCase(TestCase):
             'description': 'Описание сервиса',
             'price': '300',
             'time': '60'
-        }, follow=True)
+        })
         service = Service.objects.filter(title='Сервис 1')
         self.assertEquals(len(service), 1)
 
@@ -35,7 +35,20 @@ class ManagerTestCase(TestCase):
             'rang': '0',
             'status': '0',
             'service_1': '1'
-        }, follow=True)
+        })
         master = Master.objects.filter(name='Мастер 1')
         self.assertEquals(len(master), 1)
+
+
+    def test_calendar_post(self):
+        self.client.login(username='admin', password='admin')
+        master = Master.objects.create(name='Мастер 4', phone=38050695200, description='text', rang=0, status=1)
+        response = self.client.post('/user/manager/calendars/add', {
+            'master': master.id,
+            'date': '2020-12-12',
+            'time_start': '08:00',
+            'time_end': '17:00',
+        })
+        calendar = Calendar.objects.all()
+        self.assertEquals(len(calendar), 1)
 
